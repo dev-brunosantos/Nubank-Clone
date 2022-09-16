26e0c9c39a435fc8c41f0dcd7b835ccfa7608cf9
dev-brunosantos <100520104+dev-brunosantos@users.noreply.github.com>
2022-09-16 19:12:09 -0300

    Nova Atualização

--- js/validarCPF.js
+++ js/validarCPF.js
@@ -3,17 +3,26 @@ var form = document.querySelector('.inicio-formulario');
 var cpf = document.querySelector('.cpf-dado');
 const btnContinuar = document.getElementById('continuar');
 
+const mensagemDeErro = document.getElementById('cpf-invalido');
+
 
 setInterval(() => {
     validar();
 }, 300);
 
 function validar() {
+
     if(cpf.value.length >=11) {
         btnContinuar.style.backgroundColor = "var(--roxo)"
+        
+        
+        if(cpf.value!="12345678910") {
+            mensagemDeErro.textContent = "CPF inválido"
+        }
     }
     
     else {
         btnContinuar.style.backgroundColor = "var(--cinza)"
+        mensagemDeErro.textContent = ""
     }
 }
