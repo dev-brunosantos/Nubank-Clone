f58cb300de38674dce22b9459a501d3d3fb45639
dev-brunosantos <100520104+dev-brunosantos@users.noreply.github.com>
2022-09-16 22:24:14 -0300

    Validação de CPF com array

--- js/validarCPF.js
+++ js/validarCPF.js
@@ -1,3 +1,5 @@
+const dados = [12312312312, 23423423423]
+
 // FUNÇÃO DE VALIDAÇÃO DE CPF
 var form = document.querySelector('.inicio-formulario');
 var cpf = document.querySelector('.cpf-dado');
@@ -14,11 +16,18 @@ function validar() {
 
     if(cpf.value.length >=11) {
         btnContinuar.style.backgroundColor = "var(--roxo)"
+
+        if((cpf.value==dados[0])||(cpf.value==dados[1])) {
+            mensagemDeErro.style.color = "green"
+            mensagemDeErro.textContent = "CPF Verificado"
+        }
         
-        
-        if(cpf.value!="12345678910") {
+        else {
+            mensagemDeErro.style.color = "red"
             mensagemDeErro.textContent = "CPF inválido"
         }
+
+        
     }
     
     else {
